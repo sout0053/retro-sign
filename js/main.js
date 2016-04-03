4deedc03095cf2174d559925f0ead1426f35a816
sout0053 <maxwell.southwood@outlook.com>
2016-04-03 19:25:54 -0400

    trying to fix code for markbot
    it says it needs to see ‘.btn:active’ selector, I’m just trying to
    figure out where it goes, because to me, the way I have it works and it
    makes sense to me. Not sure where to put it.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var $svg = $('svg');
+/*  var $svg = $('svg');
+var $lines = $('.lines');
+var $btn = $('.btn');
+var $engage = $('.engage');
+
+$btn.on('click', function () {
+  $btn.toggleClass('btn-2');
+  $lines.toggleClass('linify');
+  $lines.toggleClass('lines');
+  $engage.toggleClass('is-engaged');
+}); */
+
 var $lines = $('.lines');
 var $btn = $('.btn');
 var $engage = $('.engage');
