41c4b9b21a36a6be090c82b06c6dd6370487eb67
sout0053 <maxwell.southwood@outlook.com>
2016-04-03 19:07:09 -0400

    added css and more javascript

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ var $svg = $('svg');
 var $lines = $('.lines');
 var $btn = $('.btn');
 var $engage = $('.engage');
+
+$btn.on('click', function () {
+  $btn.toggleClass('btn-2');
+  $lines.toggleClass('linify');
+  $lines.toggleClass('lines');
+  $engage.toggleClass('is-engaged');
+});
